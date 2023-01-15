399fae2b424a6bff5eb32a16448c9b0e5e01df07
Claudio Andre Hernandez Zavala <claudiohernandez.zavala@gmail.com>
2023-01-15 00:45:41 -0500

    Second commit with custom toast msg functionality

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,16 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       val button= findViewById<Button>(R.id.hellobutton)
-        button.setOnClickListener {
+        //hello button
+       val helloButton= findViewById<Button>(R.id.hellobutton);
+        //button to display custom message in toast
+        val customMsgButton =  findViewById<Button>(R.id.customMessageButton)
+
+
+
+        helloButton.setOnClickListener {
             Log.v("onclicklistener","button was clicked!")
+
             Toast.makeText(this,"Hello visitor! Hope you like the app",Toast.LENGTH_LONG).show()
+        }
+        customMsgButton.setOnClickListener{
+            val inputText =  findViewById<EditText>(R.id.customMessageTextField).text
+            if(inputText.isBlank()){
+                Toast.makeText(this,"Hey you should write something first!",Toast.LENGTH_LONG).show()
+
+            }else{
+                Toast.makeText(this,inputText,Toast.LENGTH_LONG).show()
+
+            }
         }
     }
 }
